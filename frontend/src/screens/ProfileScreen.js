289a1cda3db530dcc9b8fe3165b9ae34185cfb01
honggyu <zowoz0@naver.com>
2020-10-14 13:56:21 +0900

    User Details & Orders Reset

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -134,7 +134,7 @@ const ProfileScreen = ({ location, history }) => {
                   )}</td>
                   <td>
                     <LinkContainer to={`/order/${order._id}`}>
-                      <Button variant='light'>Details</Button>
+                      <Button className='btn-sm' variant='light'>Details</Button>
                     </LinkContainer>
                   </td>
                 </tr>
